9f278cac67eaaaa2ac829c9541d7d4a2f0aa0c56
左其瑞 <1204971479@qq.com>
2026-08-27 21:46:55 +0800

    upload: add RecoveredUnityProject/Assets/Scripts/ChaseState.cs

---
+++ RecoveredUnityProject/Assets/Scripts/ChaseState.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+/// <summary>在警戒范围内追逐玩家，进入攻击圈后切换为攻击状态。</summary>
+public class ChaseState : IState
+{
+	private readonly FSM manager;
+	private readonly Parameter parameter;
+
+	public ChaseState(FSM manager)
+	{
+		this.manager = manager;
+		parameter = manager.parameter;
+	}
+
+	public void OnEnter()
+	{
+		parameter.animator.Play("Walk");
+	}
+
+	public void OnUpdate()
+	{
+		if (parameter.getHit)
+		{
+			manager.TransitionState(StateType.Hit);
+			return;
+		}
+		if (!StateGuards.TargetInsideChaseRange(parameter))
+		{
+			manager.TransitionState(StateType.Idle);
+			return;
+		}
+
+		manager.FlipTo(parameter.target);
+		manager.transform.position = Vector2.MoveTowards(
+			manager.transform.position,
+			parameter.target.position,
+			parameter.chaseSpeed * Time.deltaTime);
+
+		if (parameter.attackPoint != null &&
+			Physics2D.OverlapCircle(parameter.attackPoint.position, parameter.attackArea, parameter.targetLayer) != null)
+		{
+			manager.TransitionState(StateType.Attack);
+		}
+	}
+
+	public void OnExit()
+	{
+	}
+}
