8569c2890db3d341febc8a79481b5843d1c27447
左其瑞 <1204971479@qq.com>
2026-08-27 21:48:29 +0800

    upload: add RecoveredUnityProject/Assets/Scripts/ReactState.cs

---
+++ RecoveredUnityProject/Assets/Scripts/ReactState.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+/// <summary>敌人发现玩家后的短暂警觉动作，结束后开始追击。</summary>
+public class ReactState : IState
+{
+	private readonly FSM manager;
+	private readonly Parameter parameter;
+
+	public ReactState(FSM manager)
+	{
+		this.manager = manager;
+		parameter = manager.parameter;
+	}
+
+	public void OnEnter()
+	{
+		parameter.animator.Play("React");
+	}
+
+	public void OnUpdate()
+	{
+		if (parameter.getHit)
+		{
+			manager.TransitionState(StateType.Hit);
+			return;
+		}
+		if (parameter.animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.95f)
+		{
+			manager.TransitionState(StateType.Chase);
+		}
+	}
+
+	public void OnExit()
+	{
+	}
+}
