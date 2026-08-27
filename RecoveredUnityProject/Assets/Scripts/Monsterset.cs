73d11565550ddd7bda4b2d9bb62e384ef48d2658
左其瑞 <1204971479@qq.com>
2026-08-27 21:48:00 +0800

    upload: add RecoveredUnityProject/Assets/Scripts/Monsterset.cs

---
+++ RecoveredUnityProject/Assets/Scripts/Monsterset.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+/// <summary>轻量级怪物生命组件；生命归零时销毁对象。</summary>
+public class Monsterset : MonoBehaviour
+{
+	[Min(1)]
+	public int health;
+	public int damage;
+
+	public void TakeDamage(int damage)
+	{
+		health = Mathf.Max(0, health - Mathf.Max(0, damage));
+		if (health == 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
