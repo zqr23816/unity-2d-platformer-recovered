29655984e1030016c662526b6391327e1a015e90
左其瑞 <1204971479@qq.com>
2026-08-27 21:47:46 +0800

    upload: add RecoveredUnityProject/Assets/Scripts/InitButton.cs

---
+++ RecoveredUnityProject/Assets/Scripts/InitButton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>键盘或手柄导航丢失焦点时，恢复到最近一次选择的 UI 元素。</summary>
+public class InitButton : MonoBehaviour
+{
+	private GameObject lastSelect;
+
+	private void Start()
+	{
+		lastSelect = EventSystem.current != null ? EventSystem.current.currentSelectedGameObject : null;
+	}
+
+	private void Update()
+	{
+		if (EventSystem.current == null)
+		{
+			return;
+		}
+
+		if (EventSystem.current.currentSelectedGameObject == null && lastSelect != null)
+		{
+			EventSystem.current.SetSelectedGameObject(lastSelect);
+		}
+		else
+		{
+			lastSelect = EventSystem.current.currentSelectedGameObject;
+		}
+	}
+}
