c7711d2f0cddafaba7cfa8b4e227e177966d3e88
左其瑞 <1204971479@qq.com>
2026-08-27 21:47:55 +0800

    upload: add RecoveredUnityProject/Assets/Scripts/MainMenu.cs

---
+++ RecoveredUnityProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>主菜单按钮事件：开始游戏与退出程序。</summary>
+public class MainMenu : MonoBehaviour
+{
+	public void PlayGame()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
