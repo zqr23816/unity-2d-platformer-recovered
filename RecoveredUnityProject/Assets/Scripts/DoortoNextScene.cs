b1d8bb10db85f9a1d077c1e800229b908c7bbf78
左其瑞 <1204971479@qq.com>
2026-08-27 21:47:08 +0800

    upload: add RecoveredUnityProject/Assets/Scripts/DoortoNextScene.cs

---
+++ RecoveredUnityProject/Assets/Scripts/DoortoNextScene.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>玩家离开关卡出口触发器后加载构建列表中的下一场景。</summary>
+public class DoortoNextScene : MonoBehaviour
+{
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.CompareTag("Player") && other is BoxCollider2D)
+		{
+			int nextIndex = SceneManager.GetActiveScene().buildIndex + 1;
+			if (nextIndex < SceneManager.sceneCountInBuildSettings)
+			{
+				SceneManager.LoadScene(nextIndex);
+			}
+			else
+			{
+				Debug.LogWarning("已到达最后一个关卡，未配置下一场景。");
+			}
+		}
+	}
+}
